9f16a8f7154b03d9720ba432bbc9a96b5c8bc8d9
Khen Tungnung <khen.tungnung@gmail.com>
2021-08-06 14:41:32 -0400

    created functioning Navbar with hiding nav menus depending on screen size

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,88 @@
 import React from 'react'
+import styled, {css} from 'styled-components/macro' /*added the macro to include the {css} */
+import {Link } from 'react-router-dom'
+import {menuData} from "../data/MenuData";
+import {Button} from "./Button";
+import {FaBars} from 'react-icons/fa' /* imports the bars */
+
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`
+const Logo = styled(Link)`
+  ${NavLink}; /* taken from the NavLink above. just adds the style onto here*/
+  font-style: italic;
+`
+const MenuBars = styled(FaBars)`
+  display: none;
+  
+  @media screen and (max-width: 768px){
+    display: block;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    postion: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-10%, -15%);
+  }
+`
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+  
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+`
+const NavMenuLinks = styled(Link)`
+  ${NavLink};
+`
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+`
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <Nav>
+            <Logo to="/">ELIXR</Logo>
+            <MenuBars/>
+            <NavMenu>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+            <NavBtn>
+                <Button to="/contact" primary='true'>
+                    Contact Us
+                </Button>
+            </NavBtn>
+        </Nav>
     )
 }
 
