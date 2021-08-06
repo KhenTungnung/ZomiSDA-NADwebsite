6fed9e059adf9bc55f9e8a52fef4e8a9a97802ee
Khen Tungnung <khen.tungnung@gmail.com>
2021-08-06 15:51:20 -0400

    now working on Hero.js and trying to make the images show

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Navbar from "./components/navbar";
 import GlobalStyle from "./globalStyles";
+import Hero from "./components/Hero";
+import {SliderDataOne} from "./data/SliderData";
 
 function App() {
   return (
       <>
           <GlobalStyle />
           <Navbar />
+          <Hero slides={SliderDataOne}/>   {/*to tell Hero what slides means in Hero.js. SliderDataOne is from SliderData.js */}
       </>
   );
 }
---
+++ src/data/SliderData.js
@@ -0,0 +1,33 @@
+import ImageOne from '../images/ImageOne.jpeg'
+import ImageTwo from '../images/ImageTwo.jpeg'
+import ImageThree from '../images/ImageThree.jpeg'
+
+
+export const SliderDataOne = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$7, 777, 900',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    }
+];
+
+/* can create another export const for a different set of data */
