96f12e84a1595d4193e9a64c312e0e3ef7f85be8
Khen Tungnung <khen.tungnung@gmail.com>
2021-08-08 17:58:05 -0400

    Finished the first full home page. Need to figure out how to connect pages to each other

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react"
+import React, {useState} from "react";
 import Navbar from "./components/navbar";
 import GlobalStyle from "./globalStyles";
 import Hero from "./components/Hero";
 import {SliderDataOne} from "./data/SliderData";
+import Dropdown from "./components/Dropdown";
+import InfoSection from "./components/InfoSection";
+import {InfoDataOne} from "./data/InfoData";
 
 function App() {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
   return (
       <>
           <GlobalStyle />
-          <Navbar />
+          <Navbar toggle={toggle}/>
+          <Dropdown isOpen={isOpen} toggle={toggle}/>
           <Hero slides={SliderDataOne}/>   {/*to tell Hero what slides means in Hero.js. SliderDataOne is from SliderData.js */}
+          <InfoSection {...InfoDataOne}/>
       </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import InteriorOne from '../images/InteriorOne.jpeg'
+import InteriorTwo from '../images/InteriorTwo.jpeg'
+
+export const InfoDataOne = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Lorisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatuculpa qui officia deserunt mollit anim id est laborum.',
+    paragraphTwo: 'Lor laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dosse cillum dolore eu fugiat nulla parqui officia deserunt mollit anim id est laborum.',
+    buttonLabel: 'View Homes',
+    image: InteriorOne,
+    reverse: false,
+    delay: 100
+}
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Lorisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatuculpa qui officia deserunt mollit anim id est laborum.',
+    paragraphTwo: 'Lor laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dosse cillum dolore eu fugiat nulla parqui officia deserunt mollit anim id est laborum.',
+    buttonLabel: 'View Homes',
+    image: InteriorTwo,
+    reverse: false,
+    delay: 300
+}
